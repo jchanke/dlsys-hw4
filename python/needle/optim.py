3509bb0717b6a79fca5e65795b5fa38379fd4f35
jchanke <josephc2@andrew.cmu.edu>
2025-11-16 02:58:21 -0500

    build: copy optim.py (from hw2)

--- python/needle/optim.py
+++ python/needle/optim.py
@@ -18,7 +18,13 @@ class Optimizer:
 
 
 class SGD(Optimizer):
-    def __init__(self, params, lr=0.01, momentum=0.0, weight_decay=0.0):
+    def __init__(
+        self,
+        params: list[ndl.nn.Parameter],
+        lr=0.01,
+        momentum=0.0,
+        weight_decay=0.0,
+    ):
         super().__init__(params)
         self.lr = lr
         self.momentum = momentum
@@ -27,7 +33,17 @@ class SGD(Optimizer):
 
     def step(self):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        lr = self.lr
+        wd = self.weight_decay
+        beta = self.momentum
+        for i, p in enumerate(self.params):
+            if p.grad is None:
+                continue
+            # Compute the gradient of the (loss + regulariztion term)
+            grad = p.grad.data + wd * p.data
+            u = beta * self.u.get(i, 0) + (1 - beta) * grad
+            self.u[i] = u.data
+            p.data = p - lr * self.u[i]
         ### END YOUR SOLUTION
 
     def clip_grad_norm(self, max_norm=0.25):
@@ -63,5 +79,27 @@ class Adam(Optimizer):
 
     def step(self):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        self.t += 1
+        b1, b2 = self.beta1, self.beta2
+        eps, lr, wd = self.eps, self.lr, self.weight_decay
+
+        # Compute the gradient & update momentum for each parameter
+        for i, p in enumerate(self.params):
+            if p.grad is None:
+                continue
+
+            # Grad is the gradient of the (loss + regularization term)
+            grad = p.grad.data + wd * p.data
+            u = b1 * self.m.get(i, 0) + (1 - b1) * grad
+            v = b2 * self.v.get(i, 0) + (1 - b2) * grad**2
+
+            self.m[i] = u.data
+            self.v[i] = v.data
+
+            # Unbiased momentum for u, v
+            u_hat = u / (1 - b1**self.t)
+            v_hat = v / (1 - b2**self.t)
+
+            # Update parameter
+            p.data = p - lr * (u_hat / (v_hat**0.5 + eps))
         ### END YOUR SOLUTION
